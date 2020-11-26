df3848f37296470b824e8ced5be613543b374a27
MDegener <mjf.degener@gmail.com>
2020-11-26 13:14:26 +0100

    added basic functionality for shiny app

---
+++ R/runExample.R
@@ -0,0 +1,10 @@
+#' @export
+
+runExample <- function() {
+  appDir <- system.file("shiny", package = "SpliceSelector")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `SpliceSelector`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
